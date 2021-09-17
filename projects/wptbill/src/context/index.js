d39b568c9b203b56479280a61d42fe28bda754f8
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-17 18:42:42 +0200

    007 Getting the looser

--- projects/wptbill/src/context/index.js
+++ projects/wptbill/src/context/index.js
@@ -30,8 +30,32 @@ class MyProvider extends Component {
 				autoClose: 2000,
 			})
 		} else {
-			console.log('move to stage 2')
+			this.setState(
+				{
+					stage: 2,
+				},
+				() => {
+					setTimeout(() => {
+						this.generateLooser()
+					}, 2000)
+				}
+			)
 		}
+	}
+
+	generateLooser = () => {
+		const { players } = this.state
+		this.setState({
+			result: players[Math.floor(Math.random() * players.length)],
+		})
+	}
+
+	resetGame = () => {
+		this.setState({
+			stage: 1,
+			players: [],
+			result: '',
+		})
 	}
 
 	render() {
@@ -43,6 +67,8 @@ class MyProvider extends Component {
 						addPlayer: this.addPlayerHandler,
 						removePlayer: this.removePlayerHandler,
 						next: this.nextHandler,
+						getNewLooser: this.generateLooser,
+						resetGame: this.resetGame,
 					}}
 				>
 					{this.props.children}
