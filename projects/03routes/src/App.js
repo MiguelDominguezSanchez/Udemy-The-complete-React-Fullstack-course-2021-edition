e252f9bb16c08e540cd1ded97fa8a1998c4c5e06
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-18 05:05:59 +0200

    003 The main route file

--- projects/03routes/src/App.js
+++ projects/03routes/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './components/home'
+import Post from './components/post'
+import Profile from './components/profile'
 
 const App = () => {
 	return <>Hello</>
