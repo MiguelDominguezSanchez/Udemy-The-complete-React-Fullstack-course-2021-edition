81d5c10f30271561dfde031840b0118604c97fdd
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-19 08:19:55 +0200

    004 Combine reducers

--- projects/06redux-project/src/index.js
+++ projects/06redux-project/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './store/reducers'
 
-const createStoreWithMiddleware = applyMiddleware()
-(createStore)
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
-  <Provider store{createStoreWithMiddleware}>
-    <App />,
-  </Provider>
-  document.getElementById('root')
-);
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
