a5ff1385dfec98b0433a9420c24e8760fd271871
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-19 08:38:42 +0200

    005 Creating a valid reducer

--- projects/06redux-project/src/components/home.js
+++ projects/06redux-project/src/components/home.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const Home = () => {
-    return(
-        <>
-            Home
-        </>
-    )
+	return <>Home</>
 }
 
-export default Home;
+export default Home
