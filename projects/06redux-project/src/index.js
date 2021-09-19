e8f13a58dcf1c4d571b9ec46cf770a544233a7e7
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-19 08:02:08 +0200

    06 Redux – 002 Connecting

---
+++ projects/06redux-project/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+
+const createStoreWithMiddleware = applyMiddleware()
+(createStore)
+
+ReactDOM.render(
+  <Provider store{createStoreWithMiddleware}>
+    <App />,
+  </Provider>
+  document.getElementById('root')
+);
