0f2b9b1c00197697adae888d898dd7bf35c58f3d
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-19 21:05:32 +0200

    003 Header and main layout

---
+++ projects/dailynews/src/hoc/mainLayout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { ToastContainer, toast } from 'react-toastify'
+
+const MainLayout = (props) => {
+	return (
+		<Container className='mt-5 mb-5'>
+			{props.children}
+			<ToastContainer />
+		</Container>
+	)
+}
+
+export default MainLayout
