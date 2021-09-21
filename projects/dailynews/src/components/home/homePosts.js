51b87421be8af32bed9cb0c207c7fe56c6bbe452
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-21 06:46:40 +0200

    006 Fetching home posts part 2

--- projects/dailynews/src/components/home/homePosts.js
+++ projects/dailynews/src/components/home/homePosts.js
@@ -2,15 +2,42 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getPosts } from '../../store/actions'
 
+import { Spinner, Button } from 'react-bootstrap'
+
 const HomePosts = () => {
-	const homePosts = useSelector((state) => state.posts)
+	const homePost = useSelector((state) => state.posts)
 	const dispatch = useDispatch()
+	const [loading, setLoading] = useState(false)
 
 	useEffect(() => {
-		dispatch(getPosts())
+		dispatch(getPosts({}, 1, 'desc', 6))
 	}, [dispatch])
 
-	return <>HomePosts</>
+	const loadMorePosts = () => {
+		const page = homePost.page + 1
+		setLoading(true)
+		dispatch(getPosts(homePost, page, 'desc', 6)).then(() => {
+			setLoading(false)
+		})
+	}
+
+	return (
+		<>
+			{loading ? (
+				<div style={{ textAlign: 'center' }}>
+					<Spinner animation='border' role='status'>
+						<span className='visually-hidden'>Loading...</span>
+					</Spinner>
+				</div>
+			) : null}
+
+			{!homePost.end & !loading ? (
+				<Button variant='outline-dark' onClick={() => loadMorePosts()}>
+					Load more posts
+				</Button>
+			) : null}
+		</>
+	)
 }
 
 export default HomePosts
