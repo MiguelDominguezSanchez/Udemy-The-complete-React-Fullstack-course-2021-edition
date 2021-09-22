66b3649bec16a7f697bf7701d1128514172c9848
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-22 10:35:41 +0200

    006 Formik - Custom fields

--- projects/07forms/src/formThree.js
+++ projects/07forms/src/formThree.js
@@ -4,14 +4,35 @@ import * as Yup from 'yup'
 
 const FormThree = () => {
 	const formikProps = {
-		initialValues: { firstname: '', color: '' },
+		initialValues: { firstname: '', color: '', lastname: '', age: '' },
 		validationSchema: Yup.object({
 			firstname: Yup.string().required('Sorry, this is required'),
+			lastname: Yup.string().required('Sorry, this is required'),
+			age: Yup.string().required('Sorry, this is required'),
 		}),
 		onSubmit: (values) => {
 			console.log(values)
 		},
 	}
+
+	const formikInputCustomField = ({
+		field, // {name, value, onChange, onBlur}
+		form: { touched, errors },
+		...props
+	}) => (
+		<>
+			<label htmlFor={field.name}>{props.labelName}</label>
+			<input
+				type='text'
+				className='form-control'
+				placeholder={props.placeholder}
+				{...field}
+			/>
+			{errors[field.name] && touched[field.name] ? (
+				<span>{errors[field.name]}</span>
+			) : null}
+		</>
+	)
 
 	return (
 		<Formik {...formikProps}>
@@ -35,6 +56,20 @@ const FormThree = () => {
 								<option value='green'>Green</option>
 								<option value='blue'>Blue</option>
 							</Field>
+							<hr className='mb-4' />
+							<Field
+								name='lastname'
+								component={formikInputCustomField}
+								placeholder='Last name'
+								labelName='Enter your lastname'
+							/>
+							<hr className='mb-4' />
+							<Field
+								name='age'
+								component={formikInputCustomField}
+								placeholder='put your age'
+								labelName='Enter your age'
+							/>
 							<hr className='mb-4' />
 							<button
 								className='btn btn-primary btn-lg btn-block'
