b99e09f28f3f162e9b7e411f323ab913c5056760
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-22 11:21:27 +0200

    007 Custom - Creating the state and input

--- projects/07forms/src/routes.js
+++ projects/07forms/src/routes.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
-import Header from './header';
-import FormOne from './formOne';
-import FormTwo from './formtwo';
-import FormThree from './formThree';
+import Header from './header'
+import FormOne from './formOne'
+import FormTwo from './formtwo'
+import FormThree from './formThree'
+import FormFour from './formFour'
 
 class Routes extends Component {
-
-    render(){
-        return(
-            <BrowserRouter>
-                <Header/>
-                <Switch>
-                    <Route path="/formthree" component={FormThree} />
-                    <Route path="/formtwo" component={FormTwo} />
-                    <Route path="/" component={FormOne} />
-                </Switch>
-            </BrowserRouter>
-        )
-    }
+	render() {
+		return (
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route path='/formfour' component={FormFour} />
+					<Route path='/formthree' component={FormThree} />
+					<Route path='/formtwo' component={FormTwo} />
+					<Route path='/' component={FormOne} />
+				</Switch>
+			</BrowserRouter>
+		)
+	}
 }
 
-export default Routes;
+export default Routes
